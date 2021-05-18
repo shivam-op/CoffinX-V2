db9613b2cb7349fde2eabeca5766e8de9fa7da76
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-18 17:21:41 +0530

    Create ping.py

---
+++ handlers/ping.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
+
+
+@Client.on_message(filters.command("help") & filters.private & ~filters.channel)
+async def help(_, message: Message):
+    await message.reply_text(
+        f"""**Contact me in PM to get command**""")
+        reply_markup=InlineKeyboardMarkup
+            [
+                [
+                    InlineKeyboardButton(
+                        "press here 🔰", url="t.me/Try_music_robot?start=help"
+                    )
+                ]
+            ]
+        ),
+    )
