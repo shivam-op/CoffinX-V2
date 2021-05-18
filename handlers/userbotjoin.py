13c007f9b6b8665a1bc01355679b3c908f07e6fd
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-18 16:58:10 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -39,7 +39,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "OxyXMusic"
+        user.first_name =  "CoffinXV2"
 
     try:
         await USER.join_chat(invitelink)
