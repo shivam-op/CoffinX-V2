96cb062956baeeb5d253b3d8f391d9d3bcdc4a0f
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-18 17:23:10 +0530

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
 
 
-@Client.on_message(filters.command("help") & filters.private & ~filters.channel)
+@Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
 async def help(_, message: Message):
     await message.reply_text(
         f"""**Contact me in PM to get command**""")
