e7e6d48ec2d6cf0527b068424e0e2598c30f3f62
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-18 17:48:22 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -551,7 +551,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption="**Currently Playing \n🏷 Name: {title}**".format()
+        caption="**Currently Playing \n🏷 Name: {title}**".format(
         message.from_user.mention()
         ),
     )
