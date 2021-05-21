f6124be75ad1f0ec5ea466e133467746877ddde1
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-21 17:27:07 +0530

    Create playback.py

---
+++ handlers/playback.py
@@ -0,0 +1,13 @@
+__mod_name__ = "Playback ⏯"
+
+__help__ = """
+<b>𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂<b>
+<b>playback<b>
+- /player: Open Settings menu of player
+- /skip: Skips the current track
+- /pause: Pause track
+- /resume: Resumes the paused track
+- /end: Stops media playback
+- /current: Shows the current Playing track
+- /playlist: Shows playlist
+"""
