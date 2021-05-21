07a48fd5574e56fd7d2da5945e6c32f314f01ff5
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-21 20:05:33 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, callback_data
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_NAME as bn
 
