1503a99f17195ef0cd9845471f236c2f82458a10
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-22 10:09:13 +0530

    Create stats.py

---
+++ handlers/stats.py
@@ -0,0 +1,13 @@
+
+@Client.on_message(filters.command("stats") & ~filters.private & ~filters.channel)
+async def gstats(_, message: Message):
+    await message.reply_text(
+      f"""𝙃𝙄𝙉𝘼𝙏𝘼 𝘽𝙊𝙏
+------------------
+𝙷𝙸𝙽𝙰𝚃𝙰 𝚄𝙿𝚃𝙸𝙼𝙴: {formatter.get_readable_time((bot_uptime))}
+𝙱𝙾𝚃: {round(process.memory_info()[0] / 1024 ** 2)} MB
+𝙲𝙿𝚄: {cpu}%
+𝚁𝙰𝙼: {mem}%
+𝙳𝙸𝚂𝙺: {disk}%
+𝙰 𝙱𝙾𝚃 𝙱𝚈 𝚃𝙴𝙰𝙼 𝙲𝙾𝙵𝙵𝙸𝙽 👨‍💻
+"""
