9335693ce455cde58c5ce6c4f5f2019c3ac5914b
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-22 10:46:52 +0530

    Update hinata.py

--- handlers/hinata.py
+++ handlers/hinata.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_NAME as bn
-
+from handlers import bot_sys_stats
 
 
 
