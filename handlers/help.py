6a8630208393611054603b709991b5766b276f25
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-22 10:15:03 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -1,7 +1,7 @@
 @Client.on_message(filters.command("help") & filters.group & ~filters.private & ~filters.channel)
 async def ghelp(_, message: Message):
     await message.reply_text(
-      """Hey Contact Me In Pm For Help""".
+      """**Hey Contact Me In Pm For Help**""".
         reply_markup=InlineKeyboardMarkup(
             [
                 [
