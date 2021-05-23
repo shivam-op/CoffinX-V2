10b2db5874d8b162e9585923938214a1f6dfd2e9
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-23 20:17:28 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -57,10 +57,10 @@ async def ytsearch(_, message: Message):
         await message.reply_text(str(e))
 
         
-"""       
- __MODULE__ = "Yt Search"  
 
-__HELP__ = 
-<b>𝙲𝙾𝙰𝙼𝙼𝙰𝙽𝙳𝚂<b>
+ __mod_name__ = "Yt Search"  
+
+__help__ = """
+𝙲𝙾𝙰𝙼𝙼𝙰𝙽𝙳𝚂
 - /ytsearch <name>
 """
