04ceed18453a80d42e63d0bedeb5ea9556f43d68
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-24 09:53:10 +0530

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -56,12 +56,12 @@ async def ping_pong(_, m: Message):
         f"- 𝙲𝙾𝙵𝙵𝙸𝙽 𝚄𝙿𝚃𝙸𝙼𝙴: `{uptime}`\n"
         f"- 𝚂𝚃𝙰𝚁𝚃 𝚃𝙸𝙼𝙴: `{START_TIME_ISO}`"
     )
-
+"""
 __mod_name__ = "ping/uptime"
 
-__help__ = """
+__help__ = 
 𝙿𝙸𝙽𝙶 𝙰𝙽𝙳 𝚄𝙿𝚃𝙸𝙼𝙴 
-"""
-__command_list__ = ["ping"], ["uptime"]
 
+__command_list__ = ["ping"], ["uptime"]
+"""
 
